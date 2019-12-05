feec9a8d74812b72ae0db2fd67fe91b12d5ebc3a
ofkox <kevin.planche65@gmail.com>
2019-12-05 17:34:24 +0100

    1 commit

---
+++ src/main/java/miage/kpla/bowling/App.java
@@ -0,0 +1,13 @@
+package miage.kpla.bowling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
